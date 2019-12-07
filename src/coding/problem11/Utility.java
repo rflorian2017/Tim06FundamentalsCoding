b1a46659d87a59ac5fd705be7d13e572daa85381
rflorian2017 <roby_florian@yahoo.com>
2019-12-07 10:50:00 +0200

    created package

---
+++ src/coding/problem11/Utility.java
@@ -0,0 +1,10 @@
+package coding.problem11;
+
+import java.util.Scanner;
+
+public class Utility {
+    public static int readCapacity(Scanner scanner) {
+        System.out.print("Enter number :");
+        return scanner.nextInt();
+    }
+}

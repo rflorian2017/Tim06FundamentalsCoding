06e597d63a0ace847298707fce8fa571b57f216e
Giorgiana Radoi <giorgiana.radoi@yahoo.com>
2019-12-07 11:16:05 +0200

    Punctul 1 si jumatate din  2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,7 @@
 package sample;
 
+import coding.problem11.Utility;
+
 import java.util.Scanner;
 
 import static sample.Misc.Punct11;
@@ -7,8 +9,23 @@ import static sample.Misc.Punct11;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner( System.in );
+        //That can read two numbers from the keyboard. Second is the capacity of the store in total number of products
+       int capacitate = Utility.readCapacity( scanner );
+        //Can read words representing product names from the keyboard until you reach the first number read. }
+
+        int nrProduse;
+        System.out.println("nrProduse");
+        while((nrProduse = scanner.nextInt()) > capacitate){
+            System.out.println("nrProduse must be less than capacity");
+        }
+        System.out.println(nrProduse);
+        String [] produse = new String[nrProduse];
+
+
+
+
+
+
     }
-
-
 }
